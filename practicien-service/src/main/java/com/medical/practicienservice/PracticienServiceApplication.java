2ba73e2a0e238ebfa48c0ea8f2f2c6dd4b120676
Ayoub05Chellabi <chellabi.ayoub@gmail.com>
2025-02-19 13:53:02 +0100

    First Commit

---
+++ practicien-service/src/main/java/com/medical/practicienservice/PracticienServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medical.practicienservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticienServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticienServiceApplication.class, args);
+    }
+
+}
---
+++ practicien-service/src/test/java/com/medical/practicienservice/PracticienServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.practicienservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticienServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
